b33fa3399fc3f79cfbf6f1d88d573b5b8cc615b6
Ronald Keinberger <ronald.keinberger@vendevio.com>
2021-03-20 17:47:35 +0100

    removes hello world component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,11 @@
     <roke-hero />
     <roke-skills />
     <roke-form />
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import RokeHero from "@/components/RokeHero";
 import RokeSkills from "@/components/RokeSkills";
 import RokeForm from "@/components/RokeForm";
@@ -21,7 +18,6 @@ export default {
     RokeForm,
     RokeSkills,
     RokeHero,
-    HelloWorld,
   },
 };
 </script>
