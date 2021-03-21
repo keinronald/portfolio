4082fead9554d00292296bbd86069d3acb312284
Ronald Keinberger <ronald.keinberger@vendevio.com>
2021-03-21 13:14:08 +0100

    updates nav with useful links and logo

--- src/components/RokeNav.vue
+++ src/components/RokeNav.vue
@@ -1,9 +1,16 @@
 <template>
   <nav class="nav" :class="{ 'nav--scrolled': !onTop }">
-    <div class="container container--no-padding">
-      <h2>RK-logo</h2>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container container--no-padding nav__container">
+      <router-link to="/">
+        <img
+          class="nav__logo"
+          :src="require('../assets/images/logo.png')"
+          height="50"
+          alt="Logo"
+        />
+      </router-link>
+      <router-link class="nav__link" to="/">Home</router-link>
+      <a class="nav__link" @click="scrollTo('skills')">Skills</a>
     </div>
   </nav>
 </template>
@@ -20,13 +27,22 @@ export default {
     handleScroll() {
       this.onTop = window.pageYOffset === 0;
     },
-  },
+    scrollTo(id = undefined) {
+      const headerOffset = 100;
+      const elementPosition = id
+        ? document.getElementById(id).getBoundingClientRect().top - headerOffset
+        : 0;
 
+      window.scrollTo({ top: elementPosition, behavior: "smooth" });
+    },
+  },
   created: function () {
     window.addEventListener("scroll", this.handleScroll);
+    window.addEventListener("hashchange", this.scrollTo);
   },
   destroyed: function () {
     window.removeEventListener("scroll", this.handleScroll);
+    window.addEventListener("hashchange", this.scrollTo);
   },
 };
 </script>
@@ -39,6 +55,29 @@ export default {
   width: 100%;
   -webkit-transition: all 250ms ease-in-out;
   transition: all 250ms ease-in-out;
+
+  &__logo {
+    transition: transform 250ms ease-in-out;
+    cursor: pointer;
+    margin-right: 50px;
+  }
+
+  &__container {
+    display: flex;
+    align-items: center;
+  }
+
+  &__link {
+    padding: 15px;
+    margin: 0 15px;
+    text-decoration: none;
+    cursor: pointer;
+    box-sizing: border-box;
+
+    &:hover {
+      border-bottom: 3px solid black;
+    }
+  }
 
   h2 {
     display: inline-block;
@@ -58,6 +97,10 @@ export default {
     background-color: white;
     box-shadow: 0 8px 32px 0 rgba(146, 146, 146, 0.37);
     padding: 5px 30px;
+
+    .nav__logo {
+      transform: scale(0.5, 0.5);
+    }
   }
 }
 </style>
